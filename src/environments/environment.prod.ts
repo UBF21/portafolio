4d4b6e98bedc2bff7f0fa345b7639e71d78dc1a4
UBF21 <flashgames104@gmail.com>
2024-04-12 01:12:01 -0500

    202404120111

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   BASE_URL : 'https://api.github.com',
   USER : 'UBF21',
-  TOKEN_GITHUB:'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'
+  TOKEN_GITHUB:'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'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   BASE_URL : 'https://api.github.com',
   USER : 'UBF21',
-  TOKEN_GITHUB:'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'
+  TOKEN_GITHUB:'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'
 };
 
 /*
